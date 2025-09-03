e4a9fc721a55743a01ffa578b2fed2841129e9f3
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-09-03 16:19:13 +0600

    js task completed

---
+++ task/task.js
@@ -0,0 +1,49 @@
+
+// var task1Completed = true
+// var orange = 25;
+// var apple = 35;
+// var myMoney = 100;
+
+// var fruitsPrice = orange + apple;
+// console.log(myMoney-fruitsPrice);
+
+// console.log(task1Completed);
+
+
+
+// var bangla = 85.5;
+// var math = 70;
+// var english = 95;
+// var arabic =100;
+// var fiqh = 98;
+
+// var totalMarks = bangla+math+english+arabic+fiqh;
+// var averageNumber = totalMarks/5;
+
+// console.log(totalMarks);
+
+// console.log(totalMarks/5);
+// console.log(averageNumber.toFixed(2));
+
+// var task2 = 'task2Completed';
+// console.log(task2 +'Yes');
+
+
+
+// var totalNumber = 119;
+// console.log(totalNumber%5);
+// var task3 = "task3Completed?"
+// console.log(task3+"Yes✅");
+
+
+
+// var a = isNaN("11");
+// var a = isNaN(2-10);
+
+// console.log(a);
+
+// var task4 = 'task4Completed?';
+// console.log(task4+'Yes✅')
+
+
+

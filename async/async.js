20fa7c3c85b432dc4aabb8423845a5af3b99fe2c
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-09-09 21:14:18 +0600

    async basic

---
+++ async/async.js
@@ -0,0 +1,44 @@
+// console.log('one')
+// console.log('two')
+// console.log('three')
+// console.log('four')
+// console.log('five')
+
+//------------------------------------------------------------------------
+
+
+
+// console.log('one')
+// console.log('two')
+// showThree()
+// console.log('four')
+// console.log('five')
+
+// function showThree(){
+//     console.log('three')
+// }
+
+//---------------------------------------------------------------------
+
+
+
+// console.log('one')
+// console.log('two')
+// setTimeout(showThree,5000)
+// console.log('four')
+// console.log('five')
+
+// function showThree(){
+//     console.log('three')
+// }
+
+//-----------------------------------------------------------
+
+
+console.log('one')
+console.log('two')
+setTimeout(() => {
+    console.log('three inside the timeout')
+}, 4000);
+console.log('four')
+console.log('five')

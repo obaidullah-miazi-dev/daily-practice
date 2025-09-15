a1268eb5cb5fc6a41e8bd7ed41257c7e78da64b6
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-09-15 17:31:54 +0600

    react exploring

--- explore-react-core/src/App.jsx
+++ explore-react-core/src/App.jsx
@@ -1,34 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import Todo from './todo';
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      {/* <h1>Exploring React</h1>
+      <Person></Person>
+      <Developer name="Yeasin" tech="Javascript"></Developer>
+      <Developer name="Unknown" tech="Python"></Developer> */}
+      <Todo task="Learn React" isDone={false}></Todo>
+      <Todo task="Learn Js" isDone={false}></Todo>
+      <Todo task="Learn Tailwind" isDone={true}></Todo>
     </>
+  )
+}
+
+function Person() {
+  const age = 21;
+  return (
+    <div style={{
+      border:'2px solid yellow',
+      padding:'20px',
+      fontWeight:'bolder',
+      borderRadius:'20px',
+      marginBottom:'10px'
+    }}>
+      <p>I am Obaidullah Miazi and I am {age} years old</p>
+    </div>
+  )
+}
+
+const style = {
+  border:'2px solid green',
+  borderRadius:'20px',
+  padding:'20px',
+  marginBottom:'10px'
+}
+
+function Developer({name,tech}){
+  // console.log(props)
+  return(
+    <div style={style}>
+      <h1>Developer: {name}</h1>
+      <h4>Technology: {tech}</h4>
+    </div>
   )
 }
 

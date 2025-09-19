0544eb8103e86313932776ad8ef28dae46249639
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-09-19 20:02:13 +0600

    responsiveness added

--- React-Explore/Panda-kitchen-react-practice/src/Components/Foods/Foods.jsx
+++ React-Explore/Panda-kitchen-react-practice/src/Components/Foods/Foods.jsx
@@ -7,7 +7,7 @@ const Foods = ({foodsPromise}) => {
     // console.log(foods)
     return (
         <div className='mx-auto w-11/12'>
-           <div className='grid grid-cols-4 items-center gap-5'>
+           <div className='md:grid xl:grid-cols-4 lg:grid-cols-2 items-center gap-5'>
             {
             foods.map(food => <Food food={food}></Food>)
            }

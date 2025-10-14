afcccdf935bd196347384c9502ed7465bba4f419
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-10-14 19:34:08 +0600

    register and login form style added

--- React-Explore/React-Firebase-Auth/src/Components/Login.jsx
+++ React-Explore/React-Firebase-Auth/src/Components/Login.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <h1>Login</h1>
+        <div className='flex justify-center items-center mt-40'>
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                <div className="card-body">
+                    <h1 className="text-3xl font-bold">Login Now!</h1>
+                    <form>
+                        <fieldset className="fieldset">
+                            <label className="label">Email</label>
+                            <input type="email" className="input" placeholder="Email" name='email' />
+                            <label className="label">Password</label>
+                            <input type="password" className="input" placeholder="Password" name='password' />
+                            <div><a className="link link-hover">Forgot password?</a></div>
+                            <button className="btn btn-neutral mt-4">Login</button>
+                            <p>New in Here? Please
+                                <NavLink to='/register' className='text-blue-500 font-semibold hover:text-blue-400'> Register</NavLink></p>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
--- React-Explore/React-Firebase-Auth/src/Components/Register.jsx
+++ React-Explore/React-Firebase-Auth/src/Components/Register.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            <h1>Register</h1>
+        <div className='flex justify-center items-center mt-40'>
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                <div className="card-body">
+                    <h1 className="text-3xl font-bold">Register Now!</h1>
+                    <form>
+                        <fieldset className="fieldset">
+                            <label className="label">Name</label>
+                            <input type="text" className="input" placeholder="Name" name='name' />
+                            <label className="label">Email</label>
+                            <input type="email" className="input" placeholder="Email" name='email' />
+                            <label className="label">Password</label>
+                            <input type="password" className="input" placeholder="Password" name='password'/>
+                            <button className="btn btn-neutral mt-4 mb-2">Register</button>
+                            <p>Already Have an Acount? Please
+                                <NavLink to='/login' className='text-blue-500 font-semibold hover:text-blue-400'> Login</NavLink></p>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };

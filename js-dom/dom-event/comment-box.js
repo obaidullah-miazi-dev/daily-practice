8458a0877e0bd950f8df2288377cd57160a2837d
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-10-25 21:15:33 +0600

    validation added on input field

--- js-dom/dom-event/comment-box.js
+++ js-dom/dom-event/comment-box.js
@@ -2,14 +2,18 @@ document.getElementById('comment-btn')
     .addEventListener('click', function () {
         const commentBox = document.getElementById('comment-box');
         const commentValue = commentBox.value;
-
+        console.log(commentValue);
+        if (commentValue.length == 0) {
+            return
+        }
         const commentBoxContainer = document.getElementById('comment-box-container')
-        const newComment =document.createElement('p')
+        const newComment = document.createElement('p')
+
         commentBoxContainer.appendChild(newComment)
         newComment.classList.add('comment')
         newComment.innerText = commentValue;
 
-        commentBox.value= '';
-        
-        
+        commentBox.value = '';
+
+
     })
